fd1f3646737ad9706dd6b16b60736cce6c25d10a
francoromerobrewer <francoromerobrewer12@gmail.com>
2021-09-07 19:38:17 +0300

    slider uploaded to git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import data from './data';
+import { IoIosArrowForward } from 'react-icons/io';
+import { IoIosArrowBack } from 'react-icons/io';
+import { ImQuotesRight } from 'react-icons/im';
+
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    let lastIndex = people.length - 1
+    if(index === -1){
+      setIndex(lastIndex)
+    }
+    if(index === lastIndex + 1){
+      setIndex(0)
+    }
+  }, [index, people])
+
+  useEffect(() => {
+    let valor = setInterval(() => {
+      setIndex(index + 1)
+    }, 3000);
+    return () => clearInterval(valor)
+  },[index])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className="tittle"> <span>/</span> Reviews</h2>
+      <section className="slider">
+        {
+          people.map((person, personIndex) => {
+
+            const {id, image, name, job, text} = person;
+
+            //the position variable is used as the class of the article I return
+            let position = 'nextSlide';
+
+            if(personIndex === index){
+              position = 'activeSlide'
+            }
+            if(personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)){
+              position = 'lastSlide'
+            }
+
+
+            return(
+              <article className={position} key={id}>
+                <img src={image} alt={name} className="person-img"/>
+                <h3 className="person-name">{name}</h3>
+                <p className="person-job">{job}</p>
+                <p className="person-description">{text}</p>
+              </article>
+            )
+          })
+        }
+        <div className="arrows">
+          <IoIosArrowBack className="icon" onClick={() => setIndex(index - 1)}/>
+          <IoIosArrowForward className="icon" onClick={() => setIndex(index + 1)}/>
+        </div>
+        <div className="quotes">
+          <ImQuotesRight className="icon quotes"/>
+        </div>
+
+      </section>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,30 @@
+import foto1 from './images/foto1.jpg';
+import foto3 from './images/foto3.jpg';
+import foto4 from './images/foto4.jpg';
+
+const personas = [
+    {
+        id: 1,
+        name: 'John Prexler',
+        job: ' Electronic Engineer',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eos odio maiores, earum, dicta error doloremque necessdsad sdasdasnl ksjksjdksjdks sitatibus nisi quasi, deleniti praesentium sapiente at suscipit porro saepe facilis eligendi minus aspernatur.',
+        image: foto3
+    },
+    {
+        id: 2,
+        name: 'Susan McWire',
+        job: ' Office Manager',
+        text: 'Lorem ipsum dolor sit a, earum, dicta error doloremque necessitatibus nisi quasi, deleniti praesentium sapiente at suscipit porro saepe facilis eligendi minus aspernatur.',
+        image: foto1
+    },
+    {
+        id: 3,
+        name: 'Megan Styles',
+        job: 'Executive CEO',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eos odio maiores, earum, dicta error doloremque necessitatibus nisi quasi, deleniti praesentium sapiente at suscipit porro saepe facilis eligendi minus aspernatur.',
+        image: foto4
+    }
+    
+]
+
+export default personas;
